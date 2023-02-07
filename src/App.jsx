0aa74dcf7f55d06e4ab66e075f6a897eb1117bd6
Matias Pinto <pintomatias2@gmail.com>
2023-02-06 21:53:09 -0300

    card to show pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,60 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+
 function App() {
   const [count, setCount] = useState(0);
   const [pokemons, setPokemons] = useState("");
 
   useEffect(() => {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${count}/`) // https://pokeapi.co/api/v2/pokemon?limit=20&offset=0
-      .then(response => response.json())
-      .then(pokemons => {
-        console.log(pokemons)
-        setPokemons(pokemons)
-      })
+    fetch(`https://pokeapi.co/api/v2/pokemon/${count}/`)
+      .then((response) => response.json())
+      .then((pokemons) => {
+        console.log(pokemons);
+        setPokemons(pokemons);
+      });
   }, [count]);
-
-  /* useEffect(() => {
-    fetch('')
-      .then(response => response.json())
-      .then(pokemons => console.log(pokemons.results))
-  }, [pokemons]); */
 
   return (
     <div className="App">
       <h1>Poke API</h1>
       <div className="card">
-        <p>App simple para probar Vite (Poke API)</p>
+        <p>
+          App simple para probar
+          <a href="https://vitejs.dev/" target="_blank">
+            Vite
+          </a>
+          Poke API
+        </p>
         <button onClick={() => setCount((count) => count + 1)}>
           Pokemon número: {count}
         </button>
         <br />
+        <br />
         <div>
-          {count && <p>Messages: {count}</p>}
-          {pokemons && <p>{pokemons.name}</p>}
-          {pokemons && <img src={pokemons.sprites.front_default} className="" alt="imagen-pokemon" />}
+          {pokemons && (
+            <CardActionArea>
+              <Card sx={{ maxWidth: 345 }} className="card">
+                <CardMedia
+                  component="img"
+                  height="280"
+                  image={pokemons.sprites.front_default}
+                  alt="imagen pokemon"
+                  className="pokemon-card"
+                />
+                <CardContent className="pokemon-card">
+                  <Typography gutterBottom variant="h5" component="div">
+                    {pokemons.name}
+                  </Typography>
+                </CardContent>
+              </Card>
+            </CardActionArea>
+          )}
         </div>
       </div>
     </div>
